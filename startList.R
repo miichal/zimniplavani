8876ffa04e787f01de34d821e41802d4e08d39ab
root <root@dellNTB.Tea>
2018-10-28 13:10:04 +0100

    First functional event result data frame

--- startList.R
+++ startList.R
@@ -2,14 +2,27 @@
 # start list generator
 
 # reading data from zimni plavani
-install.packages("rlist")
+#install.packages("rlist")
+
 library(XML)
 library(RCurl)
 library(rlist)
-theurl <- getURL("https://bodovani.zimni-plavani.info/soutez_vysl.php?id=3754",.opts = list(ssl.verifypeer = FALSE) )
-tables <- readHTMLTable(theurl)
-tables <- list.clean(tables, fun = is.null, recursive = FALSE)
-n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+
+
+theurl <- getURL("https://bodovani.zimni-plavani.info/soutez_vysl.php?id=3755",.opts = list(ssl.verifypeer = FALSE) )
+tables <- readHTMLTable(theurl, header = TRUE, as.data.frame = TRUE)
+#tables <- list.clean(tables, fun = is.null, recursive = FALSE)
+#n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+
+#create data frame for swimmers and rank
+
+swimmers<-data.frame()
+
+sw1<-as.data.frame(tables[[1]])
+sw1<-sw1[-c(1,3,4), ]
+colnames(sw1)<-as.character(unlist(sw1[1, ]))
+
+sw1<-sw1[-1, ]
 
 # creating start list for organizers
 
