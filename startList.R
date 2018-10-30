038501eef1dc1fd4e01768022d3ee7ff20dacbfb
Michal Pohorely <miichal@dellNTB.Tea>
2018-10-30 21:11:14 +0100

    Merging and list of events

--- startList.R
+++ startList.R
@@ -9,20 +9,55 @@ library(RCurl)
 library(rlist)
 
 
-theurl <- getURL("https://bodovani.zimni-plavani.info/soutez_vysl.php?id=3755",.opts = list(ssl.verifypeer = FALSE) )
+#download data from web
+theurl <- getURL("https://bodovani.zimni-plavani.info/souteze.php?rok=2017",.opts = list(ssl.verifypeer = FALSE) )
 tables <- readHTMLTable(theurl, header = TRUE, as.data.frame = TRUE)
 #tables <- list.clean(tables, fun = is.null, recursive = FALSE)
 #n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
 
 #create data frame for swimmers and rank
 
-swimmers<-data.frame()
+#swimmers<-data.frame()
 
+#convert table to dataframe and clean
+events<-as.data.frame(tables[[1]])
+events<-events[-c(1:5), ]
+#events<-events[-c(nrow(events),nrow(events)-1,nrow(events)-2), ]
+
+colnames(sw1)<-c("umisteni","s.c.","jmeno","rocnik","oddil","VS","trat","charakter","cas","body")
+sw1<-sw1[-1, ]
+
+#download data from web
+theurl <- getURL("https://bodovani.zimni-plavani.info/soutez_vysl.php?id=3756",.opts = list(ssl.verifypeer = FALSE) )
+tables <- readHTMLTable(theurl, header = TRUE, as.data.frame = TRUE)
+#tables <- list.clean(tables, fun = is.null, recursive = FALSE)
+#n.rows <- unlist(lapply(tables, function(t) dim(t)[1]))
+
+#create data frame for swimmers and rank
+
+#swimmers<-data.frame()
+
+#convert table to dataframe and clean
 sw1<-as.data.frame(tables[[1]])
 sw1<-sw1[-c(1,3,4), ]
-colnames(sw1)<-as.character(unlist(sw1[1, ]))
+sw1<-sw1[-c(nrow(sw1),nrow(sw1)-1,nrow(sw1)-2), ]
 
+colnames(sw1)<-c("umisteni","s.c.","jmeno","rocnik","oddil","VS","trat","charakter","cas","body")
 sw1<-sw1[-1, ]
 
 # creating start list for organizers
+
+#check maximum length of track and create subset
+maxTrat<-as.character(sw1$trat[1])
+maxTratList<-sw1[sw1$trat %in% maxTrat,]
+
+#typeof(sw1$cas)
+
+#extract just name and rank
+
+startListJicin<-maxTratList[,c("jmeno","umisteni")]
+
+startList<-merge(startListSlavkov,startListJicin,by = "jmeno", all = TRUE)
+
+
 
